3f6fb5e6915edca219144ae66cd67730b2cdcc63
TheBull963 <31750641+TheBull963@users.noreply.github.com>
2019-11-12 18:47:41 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Copy config files into home directory
+cp .zshrc ~/.zshrc
+cp .tmux.conf ~/.tmux.conf
+
+# Upgrade OS with non-interactive apt
+sudo apt update
+sudo apt remove apt-listchanges -y
+DEBIAN_FRONTEND=noninteractive sudo apt upgrade -y
+
+# Install packages
+sudo apt install -y tmux htop vim zsh
+
+# Change shell to Zsh
+chsh -s $(which zsh) $(whoami)
+
+# Setup Tmux plugin manager from scratch (comes with tmux sensible plugin)
+rm -rf ~/.tmux/plugins/
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Setup zsh theme PowerLevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+# Reminder to refresh env and reboot
+echo "Press prefix+R to ensure tmux plugins are downloaded and then reboot to ensure all changes are applied."
